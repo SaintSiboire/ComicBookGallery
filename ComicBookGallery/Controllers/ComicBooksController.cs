dda449c1ecf8650d15070d98d3b50c2a9a75888e
Jonanthan Bélanger <joe_belanger@hotmail.com>
2018-03-25 00:43:13 -0400

    Updated Models Data Controller and Views

--- ComicBookGallery/Controllers/ComicBooksController.cs
+++ ComicBookGallery/Controllers/ComicBooksController.cs
@@ -3,15 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ComicBookLibraryManager.Models;
+using ComicBookLibraryManager.Data;
 
 namespace ComicBookGallery.Controllers
 {
 	public class ComicBooksController : Controller
 	{
+
 		public ActionResult Detail()
 		{
+			using (var context = new Context())
+			{
+				var comicBooks = context.ComicBooks.ToList();
+				var comicBookTitle = comicBooks.FirstOrDefault(c => c.Id == 1).Description;
+				var comicBookIssueNumber = comicBooks.FirstOrDefault(c => c.Id == 1).IssueNumber;
 
-			ViewBag.SeriesTitle = "The Amazing Spider-Man";
+				ViewBag.SeriesTitle = comicBookTitle;
+				ViewBag.IssueNumber = comicBookIssueNumber;
+				ViewBag.DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! " +
+									"Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
+				ViewBag.Artists = new string[]
+				{
+				"Script: Dan Slott",
+				"Pencils: Humberto Ramos",
+				"Inks: Victor Olazaba",
+				"Colors: Edgar Delgado",
+				"Letters: Chris Eliopoulos"
+				};
+
+				return View();
+			}
+		}
+
+		public ActionResult Index()
+		{
+
+			ViewBag.SeriesTitle = "The Index";
 			ViewBag.IssueNumber = 700;
 			ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! " +
 								"Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
@@ -27,5 +55,6 @@ namespace ComicBookGallery.Controllers
 			return View();
 
 		}
+
 	}
 }
